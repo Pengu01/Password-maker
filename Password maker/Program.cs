8685560495ad61627f5bc7eb4d85eb731912ef83
Alfons Stoltz <alfons.stoltz@learnet.se>
2021-10-07 13:39:34 +0200

    Add project files.

---
+++ Password maker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Password_maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random rnd = new Random();
+                string password = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+                Console.WriteLine("How many characters in password?");
+                try
+                {
+                    int times = Convert.ToInt32(Console.ReadLine());
+                    for(int i = 0; i < times; i++)
+                    {
+                        Console.Write(password[rnd.Next(password.Length)]);
+                    }
+                    Console.WriteLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Try again");
+                    continue;
+                }
+            }
+        }
+    }
+}
